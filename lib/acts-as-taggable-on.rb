2049427cfed287bbf20644014b938719def2521d
Jonathan del Strother <jon.delStrother@audioboo.fm>
2012-03-09 00:30:38 +0000

    Delay loading ActiveRecord until necessary

--- lib/acts-as-taggable-on.rb
+++ lib/acts-as-taggable-on.rb
@@ -3,34 +3,34 @@ require "active_record/version"
 require "action_view"
 RAILS_3 = ::ActiveRecord::VERSION::MAJOR >= 3
 
-$LOAD_PATH.unshift(File.dirname(__FILE__))
+ActiveSupport.on_load(:active_record) do
+  $LOAD_PATH.unshift(File.dirname(__FILE__))
 
-require "acts_as_taggable_on/compatibility/active_record_backports" unless RAILS_3
+  require "acts_as_taggable_on/compatibility/active_record_backports" unless RAILS_3
 
-require "acts_as_taggable_on/utils"
+  require "acts_as_taggable_on/utils"
 
-require "acts_as_taggable_on/acts_as_taggable_on"
-require "acts_as_taggable_on/acts_as_taggable_on/core"
-require "acts_as_taggable_on/acts_as_taggable_on/collection"
-require "acts_as_taggable_on/acts_as_taggable_on/cache"
-require "acts_as_taggable_on/acts_as_taggable_on/ownership"
-require "acts_as_taggable_on/acts_as_taggable_on/related"
+  require "acts_as_taggable_on/acts_as_taggable_on"
+  require "acts_as_taggable_on/acts_as_taggable_on/core"
+  require "acts_as_taggable_on/acts_as_taggable_on/collection"
+  require "acts_as_taggable_on/acts_as_taggable_on/cache"
+  require "acts_as_taggable_on/acts_as_taggable_on/ownership"
+  require "acts_as_taggable_on/acts_as_taggable_on/related"
 
-#require "acts_as_taggable_on/utils"
-require "acts_as_taggable_on/acts_as_tagger"
-require "acts_as_taggable_on/tag"
-require "acts_as_taggable_on/tag_list"
-require "acts_as_taggable_on/tags_helper"
-require "acts_as_taggable_on/tagging"
+  #require "acts_as_taggable_on/utils"
+  require "acts_as_taggable_on/acts_as_tagger"
+  require "acts_as_taggable_on/tag"
+  require "acts_as_taggable_on/tag_list"
+  require "acts_as_taggable_on/tagging"
 
-$LOAD_PATH.shift
+  $LOAD_PATH.shift
 
+  extend ActsAsTaggableOn::Taggable
+  include ActsAsTaggableOn::Tagger
 
-if defined?(ActiveRecord::Base)
-  ActiveRecord::Base.extend ActsAsTaggableOn::Taggable
-  ActiveRecord::Base.send :include, ActsAsTaggableOn::Tagger
 end
 
-if defined?(ActionView::Base)
-  ActionView::Base.send :include, ActsAsTaggableOn::TagsHelper
+ActiveSupport.on_load(:action_view) do
+  require "acts_as_taggable_on/tags_helper"
+  include ActsAsTaggableOn::TagsHelper
 end
